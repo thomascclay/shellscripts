118da55b8879a6a5ecff45b31313afafee50689b
thomascclay <thomascclay@gmail.com>
2023-05-31 23:04:41 -0400

    Create pengiun.sh

---
+++ pengiun.sh
@@ -0,0 +1,3 @@
+# Install a Copy from shell util:
+
+wget http://ftp.us.debian.org/debian/pool/main/w/wl-clipboard/wl-clipboard_2.0.0-1_amd64.deb -O wl-clipboard.deb && sudo dpkg -i wl-clipboard.deb
